627be08dcf7d6608941cbbfeb8cd7cdb3c9bc8fc
Prakhar Srivastav <prakhar1989@gmail.com>
2016-12-02 14:27:33 -0500

    done with part 2 of day 1

--- src/advent_of_code_2016/day1.clj
+++ src/advent_of_code_2016/day1.clj
@@ -23,12 +23,45 @@
           [:north [x (+ y delta)]]
           [:south [x (- y delta)]]))))
 
-(defn get-dist [filename]
-  (let [input (slurp (io/resource filename))
-        steps (->> (str/split input #",") (map str/trim))
-        [_ [x y]] (reduce new-pos [:north [0 0]] steps)]
+(defn get-steps [filename]
+  (let [input (slurp (io/resource filename))]
+    (->> (str/split input #",") (map str/trim))))
+
+(defn part-1 [steps]
+  (let [[_ [x y]] (reduce new-pos [:north [0 0]] steps)]
     (+ (Math/abs x)
        (Math/abs y))))
 
+(defn get-visited-points [[a b] [c d] dir]
+  (if (or (= dir :north) (= dir :south))
+    (if (> a c)
+      (map (fn [x] [x b]) (range a c -1))
+      (map (fn [x] [x b]) (range a c)))
+    (if (> b d)
+      (map (fn [x] [a x]) (range b d -1))
+      (map (fn [x] [a x]) (range b d)))))
 
-(println (get-dist "day1-input.txt"))
+(defn new-visited [points visited]
+  (if (empty? points) visited
+    (let [point (first points)]
+      (if (contains? visited point) point
+        (recur (rest points) (conj visited point))))))
+
+(defn part-2 [input]
+  (loop [steps input
+         pos [0 0]
+         dir :north
+         visited #{}]
+    (if (empty? steps) false ; not found
+      (let [[ndir npos] (new-pos [dir pos] (first steps))
+            points (get-visited-points pos npos dir)
+            nvisited (new-visited points visited)]
+        (if (not (set? nvisited))
+          (reduce + (map #(Math/abs %) nvisited)) ;found the ans
+          (recur (rest steps) npos ndir nvisited))))))
+
+(def steps (get-steps "day1-input.txt"))
+
+(println (part-1 steps))
+(println (part-2 steps))
+
