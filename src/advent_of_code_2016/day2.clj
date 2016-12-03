8f2dc12a54550c7de2d6472a60c1bd569be7f9a1
Prakhar Srivastav <prakhar1989@gmail.com>
2016-12-02 19:59:52 -0500

    done with part 1 of day 2

--- src/advent_of_code_2016/day2.clj
+++ src/advent_of_code_2016/day2.clj
@@ -1,4 +1,29 @@
-(ns advent-of-code-2016.day1
+(ns advent-of-code-2016.day2
   (:require [clojure.java.io :as io]
             [clojure.string :as str]))
+
+(def keypad [[1 2 3] [4 5 6] [7 8 9]])
+
+(defn pos->num [[x y]]
+  (get-in keypad [(inc y) (inc x)]))
+
+(defn move [[x y] dir]
+  (cond 
+    (= dir \U) [x (max (dec y) -1)]
+    (= dir \D) [x (min (inc y) 1)]
+    (= dir \L) [(max (dec x) -1) y]
+    (= dir \R) [(min (inc x) 1) y]))
+
+(defn compute-codes [input results curr]
+  (if (empty? input) results
+    (let [res (reduce move curr (first input))]
+      (recur (rest input) (conj results res) res))))
+
+(def input 
+  (-> (slurp (io/resource "day2-input.txt"))
+      (str/split #"\n")))
+
+(def part-1 
+  (->> (compute-codes input [] [0 0])
+       (map pos->num)))
 
