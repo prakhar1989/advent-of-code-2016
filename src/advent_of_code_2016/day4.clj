c981593f73a3acdfc1a47be07fcd683ed14a4f6b
Prakhar Srivastav <prakhar1989@gmail.com>
2016-12-04 13:15:08 -0500

    part 2 day 4

--- src/advent_of_code_2016/day4.clj
+++ src/advent_of_code_2016/day4.clj
@@ -8,16 +8,39 @@
 (defn parse [line]
   (let [id (->> (re-find #"\d+" line) (read-string))
         checksum (last (re-find #"\[(\w+)\]" line))
-        code (->> (str/split line #"-") (reverse) (rest) (apply str))]
+        code (->> (str/split line #"-") (reverse) (rest) (str/join "-"))]
     {:id id :checksum checksum :code code}))
+
+(def lines (map parse input))
 
 (defn is-valid-room? [room]
   (let [counts (frequencies (:code room))
         checksum (->> (into [] counts)
                       (sort-by (fn [[k v]] [(- v) k]))
-                      (map first) (take 5) (apply str))]
+                      (map first)
+                      (take 5)
+                      (apply str))]
     (= checksum (:checksum room))))
 
 (defn part-1 []
-  (->> (map parse input) (filter is-valid-room?)
-       (map :id) (reduce +)))
+  (->> lines (filter is-valid-room?) (map :id) (reduce +)))
+
+(defn decrypt [ch shift]
+  (if (= ch \-) \space
+    (char (+ (mod (+ (- (int ch) (int \a))
+                     shift)
+                  26)
+             (int \a)))))
+
+(defn part-2 []
+  (let [decryped-lines (->> lines
+                            (mapv (fn [line]
+                                   (->> (:code line)
+                                        (map #(decrypt % (:id line)))
+                                        (apply str)))))
+        query (->> decryped-lines
+                   (filter #(str/includes? % "north"))
+                   (first))
+        idx (.indexOf decryped-lines query)]
+    (:id (nth lines idx))))
+
