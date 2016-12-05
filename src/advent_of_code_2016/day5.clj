88f8089f38efedd69967d5b71aec3421daf28579
Prakhar Srivastav <prakhar1989@gmail.com>
2016-12-05 17:18:06 -0500

    added animation for day 5

--- src/advent_of_code_2016/day5.clj
+++ src/advent_of_code_2016/day5.clj
@@ -3,12 +3,9 @@
            (java.math BigInteger)))
 
 (defn md5 [s]
-  (let [algorithm (MessageDigest/getInstance "MD5")
-        size (* 2 (.getDigestLength algorithm))
-        raw (.digest algorithm (.getBytes s))
-        sig (.toString (BigInteger. 1 raw) 16)
-        padding (apply str (repeat (- size (count sig)) "0"))]
-    (str padding sig)))
+  (apply str (map (partial format "%02x")
+                  (.digest (doto (MessageDigest/getInstance "MD5") .reset
+                             (.update (.getBytes s)))))))
 
 (defn part-1 [s]
   (loop [res [] i 0]
@@ -20,23 +17,23 @@
           (recur res (inc i)))))))
 
 (defn print-pass [m]
- (->> (range 0 8) (map #(get m (char (+ 48 %)) "-")) (apply str) (println)))
+ (->> (range 0 8)
+      (map #(get m (char (+ 48 %)) (rand-int 10)))
+      (apply str)))
 
 (defn part-2 [s]
-  (print-pass {})
   (loop [res {} i 0]
+    (print (print-pass res) "\r")
     (if (= (count res) 8) (print-pass res)
       (let [encrypted (md5 (str s i))]
         (if (and (= (take 5 (repeat \0)) (take 5 encrypted))
                  (nil? (get res (nth encrypted 5)))
                  (Character/isDigit (nth encrypted 5))
                  (< (Integer/parseInt (str (nth encrypted 5))) 8))
-          (do
-            (print-pass (assoc res (nth encrypted 5) (nth encrypted 6))) ;; found next char
-            (recur (assoc res (nth encrypted 5) (nth encrypted 6)) (inc i)))
+          (recur (assoc res (nth encrypted 5) (nth encrypted 6)) (inc i))
           (recur res (inc i)))))))
 
 ;; part 1
-(println (part-1 "ugkcyxxp"))
+;(println (part-1 "ugkcyxxp"))
 ; part 2
-(part-2 "ugkcyxxp")
+;(part-2 "ugkcyxxp")
